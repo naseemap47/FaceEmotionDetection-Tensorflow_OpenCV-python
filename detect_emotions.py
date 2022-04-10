778d9e7b3eb580506c04b67714aa69b7422a2138
NASEEM A P <naseemalassampattil@gmail.com>
2022-04-10 13:50:52 +0530

    Show example from data set using sample_images function in my_utils

--- detect_emotions.py
+++ detect_emotions.py
@@ -1,1 +1,5 @@
-print('Hello')
+from my_utils import sample_images
+
+train_img_path = '/home/naseem/PycharmProjects/FaceEmotionDetection-Tensorflow_OpenCV-python/images/train/angry/'
+
+sample_images(train_img_path)
